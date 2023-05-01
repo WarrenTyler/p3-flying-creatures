f37afa835bf8f806bc84d718cc922c7a62d979f2
WarrenT <warrentyler1024@gmail.com>
2023-05-01 17:29:59 +0100

    Create Enemy class and animate loop

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+  }
+  update() {
+    this.x++;
+    this.y++;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+const enemy1 = new Enemy();
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  enemy1.update();
+  enemy1.draw();
+
+  requestAnimationFrame(animate);
+}
+
+animate();
