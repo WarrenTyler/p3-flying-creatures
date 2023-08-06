ae56974c19822db02fc1f96ba3e6d2902980c324
WarrenT <warrentyler1024@gmail.com>
2023-08-06 19:17:33 +0100

    Add Enemy3 module, update script to use it
    * Added a new module for Enemy3 (enemy3.js), implementing enemy type Ghost with customized movement and animation.
    * Updated the HTML file to include the script for Enemy3.
    * Modified script.js to import and use the Enemy3 module instead of Enemy2 for rendering and animating enemies.

---
+++ modules/enemy3.js
@@ -0,0 +1,50 @@
+export class Enemy3 {
+  constructor() {
+    this.image = new Image();
+    this.image.src = "img/enemy3.png";
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
+    this.amplitude = Math.random() * 7;
+  }
+  update(gameFrame) {
+    // Endless horizontal movement
+    this.x -= this.speed;
+    // Wrap around screen
+    if (this.x < -this.width) {
+      this.x = canvas.width;
+    }
+    // Vertical sine wave movement
+    this.y += this.amplitude * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
+
+    // Animate sprites
+    // Check if gameFrame is even (divisible by 2)
+    // This is a quick way to slow down animation without using deltaTime
+    if (gameFrame % this.flapSpeed === 0) {
+      // Increment this.frame by 1 and ensure it cycles through values 0 to 4
+      this.frame = (this.frame + 1) % 5;
+    }
+  }
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
